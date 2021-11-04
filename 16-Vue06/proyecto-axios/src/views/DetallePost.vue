b897b0d5866cccb3b3a43103c0dfae41ae450b3c
yaattzy <yeratzyen@gmail.com>
2021-11-04 09:56:11 -0700

    Proyecto axios-vuex

---
+++ 16-Vue06/proyecto-axios/src/views/DetallePost.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h1>Detalle</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'DetallePost'
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ 16-Vue06/proyecto-axios/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <h1>Mis Posts</h1>
+    <div v-for="p in posts" :key="p.id" class="alert alert-secondary mb-2" role="alert">
+      <h4>{{p.title}}</h4>
+      <p>{{p.body}}</p>
+      <router-link :to="`/post/${p.id}`">Ver más...</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+
+export default {
+  name: "Home",
+  components: {},
+  methods: {
+    ...mapActions(["getPosts"]),
+  },
+  computed: {
+    ...mapState(["posts"]),
+  },
+  created() {
+    this.getPosts();
+  },
+};
+</script>
