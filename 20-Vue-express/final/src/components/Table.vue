518fc37848a24a45a446d1bf70219bb9ffcb88ab
yaattzy <yeratzyen@gmail.com>
2021-11-22 09:57:06 -0700

    Visualizar CRUD

---
+++ 20-Vue-express/final/src/components/Table.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+       <b-table striped :items="items" :fields="fields"></b-table>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Tabla',
+    props: {
+        items: Array,
+        fields: {
+            type: Array,
+            default: []
+        }
+    },
+    data() {
+      return {
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 20-Vue-express/final/src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home">
+    <Table :items="personas" :fields="campos" />
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+import Table from "../components/Table.vue";
+export default {
+  name: "Home",
+  components: {
+    Table,
+  },
+  data() {
+    return {
+      campos: [
+        { key: "id", label: "Clave" },
+        "nombre",
+        {
+          key: "direccion",
+          label: "Dirección",
+          formatter: (value) => {
+            return value || "Sin información";
+          },
+        },
+        {
+          key: "telefono",
+          label: "Teléfono",
+          formatter: (value) => {
+            return value || "--";
+          },
+        },
+      ],
+    };
+  },
+  computed: {
+    ...mapState(["personas"]),
+  },
+  methods: {
+    ...mapActions(["setPersonas"]),
+  },
+  created() {
+    this.setPersonas();
+  },
+};
+</script>
